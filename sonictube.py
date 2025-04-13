d874b86cfe44491dc7934843b7b931a55345ae09
trilliongamestudio <lovyjassi8@gmail.com>
2025-04-13 13:52:32 +0530

    Update sonictube.py

--- sonictube.py
+++ sonictube.py
@@ -25,6 +25,4 @@ def handle_download():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=10000)
 
